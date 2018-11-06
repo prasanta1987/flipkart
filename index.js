17085d75f0627005b6bc5f0b6dbfed997e5be67f
prasanta1987 <prasanta.1987@hotmail.com>
2018-11-06 20:35:13 +0530

    try6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const http = require('http');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
       res.statusCode = 200;
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     var options = {
         method: 'GET',
         url: 'https://affiliate-api.flipkart.net/affiliate/1.0/search.json',
-        qs: { query: search, resultCount: '1' },
+        qs: { query: search },
         headers:
         {
             'Postman-Token': '8c04b704-23a4-4cc7-af5e-fd7f0c46743f',
